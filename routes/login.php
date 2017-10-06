dbb0e9c7d625f5cecb3ae36a1fda22ad15544795
Nat Evatt <natevatt@gmail.com>
2017-10-05 22:14:32 -0700

    created token reading from google sign in env loading credentials and auth token reading

---
+++ routes/login.php
@@ -0,0 +1,28 @@
+<?php
+
+use Lcobucci\JWT\ValidationData;
+use Lcobucci\JWT\Parser;
+
+
+//Token
+$app->get('/token', function () use ($app) {
+    $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
+    // $data->setIssuer('accounts.google.com');
+    $data->setIssuer(env('googleIssuer', false));
+    $data->setAudience(env('googleAudience', false));
+    // $data->setAudience('357217477337-f2ifm717ikgaobrp8oqjtp3hs45d4ib0.apps.googleusercontent.com');
+    // $data->setId('4f1g23a12aa');
+
+    //create token from string
+    // $token = app()->request->header('Authorization');
+    $token = app()->request->input('token', app()->request->bearerToken());
+    //validate token
+    $token = (new Parser())->parse((string) $token); // Parses from a string
+
+    // echo "This is the header " . $token->getHeader('name');
+    echo "THis is the claim" . $token->getClaim('name');
+
+    var_dump($token->validate($data));
+  // $thisVar = "Yo What up Token Friend!";
+  return $token;
+});
