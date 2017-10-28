85f201e32f273145fc190801e6954b3cde417201
Nat Evatt <natevatt@gmail.com>
2017-10-28 10:35:53 -0700

    updated mapstyle calls so that user_id is not shared over network

--- app/Models/MapStyles.php
+++ app/Models/MapStyles.php
@@ -8,18 +8,25 @@ class MapStyles
 {
     public function getMapStyles()
     {
-        $query = "SELECT * FROM maps.mapstyles ORDER BY name";
-        $data = DB::select($query);
+        $user = app()->request->user();
+        $query = DB::table('maps.mapstyles');
+        //if not logged in return all except for user_id
+        if($user){
+            $query->select('*');
+        } else {
+            $query->select('id', 'name', 'url', 'type', 'author', 'image', 'github', 'jsonStyle', 'date');
+        }
+        $query->orderBy('name');
+        $data = $query->get();
+
         //if logged in return array with editable field
         $user = app()->request->user();
         if($user){
             foreach($data as $mapStyle){
                 $mapStyle->editable = ($mapStyle->user_id == $user->user_id) ? true : false;
+                unset($mapStyle->user_id);
             }
         }
-        // else {
-        //     echo "I am not loogged in";
-        // }
         return $data;
     }
 
